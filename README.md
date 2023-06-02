a57606ded1f4782d9e20a28bce6c82bfdd5bf32c
Cyrus Beer <cyrus@go-maestro.com>
2023-06-02 10:31:51 -0400

    updated plugins and toolbar

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ Follow the guides available on https://ckeditor.com/docs/ckeditor5/latest/framew
 ## FAQ
 | Where is the place to report bugs and feature requests?
 
-You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `4jzcfurvihoj-eq35pxwy1r0f`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
+You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `j14qrysem7qo-g48ljuie9h4x`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
 
 | Where can I learn more about the CKEditor 5 framework?
 
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -2,39 +2,54 @@
  * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import DocumentList from "@ckeditor/ckeditor5-list/src/documentlist.js";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
+import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import Mention from "@ckeditor/ckeditor5-mention/src/mention.js";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
+import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat.js";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
-	Bold,
-	Essentials,
-	Italic,
-	Link,
-	Mention,
-	Paragraph
+  Alignment,
+  Bold,
+  DocumentList,
+  Essentials,
+  Indent,
+  Italic,
+  Link,
+  Paragraph,
+  RemoveFormat,
+  Underline,
 ];
 
 // Editor configuration.
 Editor.defaultConfig = {
-	toolbar: {
-		items: [
-			'bold',
-			'italic',
-			'link',
-			'|',
-			'undo',
-			'redo'
-		]
-	},
-	language: 'en'
+  toolbar: {
+    items: [
+      "bold",
+      "italic",
+      "underline",
+      "|",
+      "link",
+      "|",
+      "alignment",
+      "|",
+      "bulletedList",
+      "numberedList",
+      "|",
+      "removeFormat",
+    ],
+  },
+  language: "en",
 };
 
 export default Editor;
